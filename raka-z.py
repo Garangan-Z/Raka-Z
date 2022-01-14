3a9ff3169a204b83a29b096aaaee735d9dd4fd95
☆ RAKA ☆™︻®╤──────═◍➤ <95731529+Garangan-Z@users.noreply.github.com>
2022-01-14 14:17:38 +0700

    Update raka-z.py

--- raka-z.py
+++ raka-z.py
@@ -122,7 +122,8 @@ def raka_z():
         print '%s *%s Setelah Itu Klick %sTambahkan Ke Chrome Selesai'%(B,N,H);time.sleep(2)
         print '%s *%s Login Akun Fb Dikiwi Lalu Klick %sTitik Tiga Dipojok Kanan Atas%s Tinggal Diklick Paling Bawah Get Cookie %sSelesai%s Pahamkan'%(B,N,H,N,H,N);time.sleep(2)
         raw_input(' %s◍➤%s Tekan Enter '%(O,N))
-        os.system('xdg-open https://m.facebook.com/composer/ocelot/async_loader/?publisher=feed#_=_')
+        print '%s *%s Jangan Lupa Follow Fb Ku Ya BozZ '%(B,N)
+        os.system('xdg-open https://facebook.com/PEMUDA.KALEUM')
         raka_z()
     try:
         nama = requests.get('https://graph.facebook.com/me?access_token=%s'%(amanda)).json()['name']
